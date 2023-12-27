b99f42458766d052030f2ae176ace08f8a139f8c
aswin-setiadi <aswin.setiadi@gmail.com>
2023-12-27 15:39:53 +0800

    add roles class

---
+++ main/roles.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from skills import BodyAttributes, Skills, SoulAttributes
+
+
+class Roles(Enum):
+    ChiefMedicalDoctor = (Skills.Diagnoser,)
+    Manager = (Skills.Organizer, Skills.Consoler, Skills.Pacifier)
+    Broker = (Skills.JudgeOfIntent, Skills.Appraiser, Skills.Negotiator)
+    BookKeeper = (Skills.RecordKeeper,)
+    ExpeditionLeader = (Skills.Consoler, Skills.Pacifier)
+    MillitaryLeader = (
+        Skills.Leader,
+        Skills.Tactician,
+        Skills.ArmorUser,
+        Skills.Fighter,
+        Skills.Dodger,
+    )
+    Crafter = (
+        Skills.Cook,
+        Skills.Bowyer,
+        Skills.Carpenter,
+        Skills.StoneCarver,
+        Skills.StoneCrafter,
+        Skills.WeaponSmith,
+        Skills.GemCutter,
+        Skills.WoodCrafter,
+        Skills.Engraving,
+        Skills.Mason,
+    )
+    Doctor = (Skills.BoneDoctor, Skills.Surgeon)
+
+    @staticmethod
+    def get_role_attributes(role: "Roles") -> set[BodyAttributes | SoulAttributes]:
+        res: set[BodyAttributes | SoulAttributes] = set()
+        for skill in role.value:
+            _ = {x for x in skill.value.attributes.keys()}
+            res = res.union(_)
+        return res
+
+
+if __name__ == "__main__":
+    # print(Roles.Broker.value)  # will print tuple of Skills for Broker
+    # print(list(Roles))
+    res = Roles.get_role_attributes(Roles.MillitaryLeader)
+    print(res)
