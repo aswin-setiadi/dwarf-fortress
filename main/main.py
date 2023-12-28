36a720cd8bca0767675be636f01098c3b59e0b01
aswin-setiadi <aswin.setiadi@gmail.com>
2023-12-28 21:50:28 +0800

    add main

---
+++ main/main.py
@@ -0,0 +1,24 @@
+import logging
+
+from char import Character
+from stats import (
+    BodyAttributes as BA,
+    SoulAttributes as SA,
+    Beliefs,
+    Goals,
+    Facets,
+    AttributeType,
+)
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def generate_characters():
+    char1 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char2 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char3 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char4 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char5 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char6 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
+    char7 = Character(name="Aban", beliefs={}, goals=[], facets={}, attributes={})
