cb8438e8c2163c857b6e0f2c02319639f43c3572
aswin-setiadi <aswin.setiadi@gmail.com>
2024-01-18 08:11:07 +0800

    update printing alignment

--- main/utils.py
+++ main/utils.py
@@ -11,24 +11,34 @@ def print_skill_table(chars: list[Character], skills: Iterable[Skills] = []):
         skills = Skills
     for skill in skills:
         if skill.name == "Brewer":
+            print("".ljust(50, "#"))
             print("Orderlies".ljust(20, " ") + namestr)
         elif skill.name == "Bowyer":
+            print("".ljust(50, "#"))
             print("Crafting".ljust(20, " ") + namestr)
         elif skill.name == "Discipline":
+            print("".ljust(50, "#"))
             print("Military".ljust(20, " ") + namestr)
         elif skill.name == "Appraiser":
+            print("".ljust(50, "#"))
             print("Broker".ljust(20, " ") + namestr)
         elif skill.name == "Organizer":
+            print("".ljust(50, "#"))
             print("Mngr/ExpLdr".ljust(20, " ") + namestr)
         elif skill.name == "Diagnoser":
+            print("".ljust(50, "#"))
             print("CMD".ljust(20, " ") + namestr)
         elif skill.name == "BoneDoctor":
+            print("".ljust(50, "#"))
             print("Doctor".ljust(20, " ") + namestr)
         elif skill.name == "RecordKeeper":
+            print("".ljust(50, "#"))
             print("BookKeeper".ljust(20, " ") + namestr)
         elif skill.name == "Comedian":
+            print("".ljust(50, "#"))
             print("SocialSkills".ljust(20, " ") + namestr)
         elif skill.name == "Concentration":
+            print("".ljust(50, "#"))
             print("Misc.".ljust(20, " ") + namestr)
         l: list[str] = []
         for char in chars:
@@ -40,7 +50,8 @@ def print_skill_table(chars: list[Character], skills: Iterable[Skills] = []):
                     tmp = f"{t[0]} F {t[2]}"
                 tmp = f"|{tmp:<10}"
             else:
-                tmp = f"|None".ljust(10)
+                none = "None"
+                tmp = f"|{none:<10}"
             l.append(tmp)
         s = "".join(l)
         print(f"{skill.name:<20}{s}")
